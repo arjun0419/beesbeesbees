f3a5f4327a84e76cdef7e88613200e92e247c3bc
Arjun Chemparathy <arjunchemparathy@gmail.com>
2018-01-22 11:52:44 -0800

    add HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,22 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(color) {
+	Bee.call(this, this.color);
+	this.age = 10;
+	this.job = "make honey";
+	this.color = this.color;
+	this.honeyPot = 0;
+
+
+
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.makeHoney = function(){
+	this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function(){
+	this.honeyPot--;
+};
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
