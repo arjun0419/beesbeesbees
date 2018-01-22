1cae8deac09e5788e17e8921871188d22673d273
Arjun Chemparathy <arjunchemparathy@gmail.com>
2018-01-22 12:09:42 -0800

    add retiredForager class

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,21 @@
 var RetiredForagerBee = function() {
+	ForagerBee.call(this, this.treasureChest);
+	this.age = 40;
+	this.job = "gamble";
+	this.canFly = false;
+	this.color = "grey";
+	this.treasureChest = this.treasureChest;
+
+
 };
 
+RetiredForagerBee.prototype = Object.create(Grub.prototype);
+RetiredForagerBee..prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function(){
+	return "I am too old, let me play cards instead";
+}
+
+RetiredForagerBee.prototype.gamble = function(treasure){
+	this.treasureChest.push(treasure);	
+}
